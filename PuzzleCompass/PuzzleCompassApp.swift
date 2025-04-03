e15fcc072b28a2c378b2c39861b98f5a9288d800
masheal2333 <132886930+masheal2333@users.noreply.github.com>
2025-04-03 21:23:50 +0800

    Initial Commit

---
+++ PuzzleCompass/PuzzleCompassApp.swift
@@ -0,0 +1,17 @@
+//
+//  PuzzleCompassApp.swift
+//  PuzzleCompass
+//
+//  Created by Sheng Ma on 4/3/25.
+//
+
+import SwiftUI
+
+@main
+struct PuzzleCompassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PuzzleCompassTests/PuzzleCompassTests.swift
@@ -0,0 +1,17 @@
+//
+//  PuzzleCompassTests.swift
+//  PuzzleCompassTests
+//
+//  Created by Sheng Ma on 4/3/25.
+//
+
+import Testing
+@testable import PuzzleCompass
+
+struct PuzzleCompassTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
